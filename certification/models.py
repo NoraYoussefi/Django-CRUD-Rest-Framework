241eb7d1f1acc60663577b87a452a291e15414d2
Nora Youssefi <80409227+NoraYoussefi@users.noreply.github.com>
2022-04-23 13:44:47 +0000

    first commit

---
+++ certification/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class competition_model(models.Model):
+    title = models.CharField(max_length=70, blank=False, default='')
+    description = models.CharField(max_length=200,blank=False, default='')
+    bg_photo = models.CharField(max_length=200,blank=False, default='')
+    user_name = models.CharField(max_length=20, blank=False, default='')
+    date = models.CharField(max_length=20, blank=False, default='')
+   
+   
+    # certificat   user  competition
+# class competition_model(models.Model):
+#     title = models.CharField(max_length=70, blank=False, default='')
+#     description = models.CharField(max_length=200,blank=False, default='')
+#     bg_photo = models.CharField(max_length=200,blank=False, default='')
+    
